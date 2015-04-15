54b14439ca8593d2af281cd3120857b4ffca6c97
elite5472 <guiborges@live.com>
2015-04-15 14:20:48 -0300

    buttom.rb implemented

--- button.rb
+++ button.rb
@@ -2,10 +2,19 @@
 $LOAD_PATH.unshift File.dirname(__FILE__)
 ###
 
-load 'gpio.rb'
-#require 'gpio.rb'
+require 'gpio.rb'
 
+Port.close(14)
+Port.close(22)
 
 Port.open(14, 'w') do |p14|
-	p14.puts('1')
+	Port.open(22, 'r') do |p22|
+		while true do
+			if p22.gets == '0'
+				p14.puts(0)
+			else
+				p14.puts(1)
+			end
+		end
+	end
 end
