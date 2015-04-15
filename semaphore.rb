f9caef64cbb9d5e00a80fe926f5332bb6025e58c
elite5472 <guiborges@live.com>
2015-04-15 15:23:15 -0300

    fixes

--- semaphore.rb
+++ semaphore.rb
@@ -4,23 +4,23 @@ for i in [1, 15, 17, 18] do
 	Port.close(i)
 end
 
-leds = [Port.new(15,'w'), Port.new(17,'w'), Port.new(18,'w')]
-button = Port.new(1,'r')
+$leds = [Port.new(15,'w'), Port.new(17,'w'), Port.new(18,'w')]
+$button = Port.new(1,'r')
 
 def set_led(color)
 	case color
 	when 'green'
-		leds[0].puts(1)
-		leds[1].puts(0)
-		leds[2].puts(0)
+		$leds[0].puts(1)
+		$leds[1].puts(0)
+		$leds[2].puts(0)
 	when 'yellow'
-		leds[0].puts(0)
-		leds[1].puts(1)
-		leds[2].puts(0)
+		$leds[0].puts(0)
+		$leds[1].puts(1)
+		$leds[2].puts(0)
 	when 'red'
-		leds[0].puts(0)
-		leds[1].puts(0)
-		leds[2].puts(1)
+		$leds[0].puts(0)
+		$leds[1].puts(0)
+		$leds[2].puts(1)
 	else
 		raise "Unknown color #{color}."
 	end
