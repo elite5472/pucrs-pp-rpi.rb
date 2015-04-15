a46669347009ac6c67465b973530c61dd1a20228
elite5472 <guiborges@live.com>
2015-04-15 14:40:53 -0300

    added blink

---
+++ blink.rb
@@ -0,0 +1,19 @@
+load 'gpio.rb'
+
+Port.close(17)
+Port.close(18)
+Port.close(19)
+
+ports = [Port.open(17, 'w'), Port.open(18, 'w'), Port.open(27, 'w')]
+
+while true do
+	for p in ports do
+		p.puts(0)
+	end
+	sleep(0.5)
+	
+	for p in ports do
+		p.puts(1)
+	end
+	sleep(0.5)
+end
