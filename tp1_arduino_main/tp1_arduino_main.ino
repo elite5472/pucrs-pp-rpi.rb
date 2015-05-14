46a2a96d6c308700d1d10baa504a555985374022
lgzani <luisgzani@gmail.com>
2015-05-14 16:50:13 -0300

    Added button_event and led_event implementation

--- tp1_arduino_main/tp1_arduino_main.ino
+++ tp1_arduino_main/tp1_arduino_main.ino
@@ -25,28 +25,104 @@ int button3_count = 0;
 
 void loop()
 {
-  button_event();
   led_event();
+  button_event();
+  
 }
 
 void button_event()
 {
-  int buttonId = -1;
   if(digitalRead(BUTTON_1_PIN) == HIGH)
   {
-    buttonId
+     button1_count++;
+     if(Serial.available){
+         Serial.write(9);
+         Serial.write(-1);
+         Serial.write(2);
+         Serial.write(-1);
+         Serial.write(button1_count);
+         Serial.write(-1);
+         Serial.write(-1);
+         Serial.write(8);
+     }
   }
   else if (digitalRead(BUTTON_2_PIN) == HIGH)
   {
-    
+     button2_count++;
+     if(Serial.available){
+         Serial.write(9);
+         Serial.write(-1);
+         Serial.write(2);
+         Serial.write(-1);
+         Serial.write(-1);
+         Serial.write(button2_count);
+         Serial.write(-1);
+         Serial.write(8);
+     }
   }
   else if (digitalRead(BUTTON_3_PIN) == HIGH)
   {
-    
+     button3_count++;
+     if(Serial.available){
+         Serial.write(9);
+         Serial.write(-1);
+         Serial.write(2);
+         Serial.write(-1);
+         Serial.write(-1);
+         Serial.write(-1);
+         Serial.write(button3_count);
+         Serial.write(8);
+     }
   }
 }
 
 void led_event()
 {
-  
+     int in;
+     
+     if(Serial.available)
+     {
+         in = Serial.read(); // 9
+         
+         in = Serial.read(); // -1
+         
+         in = Serial.read(); // 1
+         if (in != 1){
+             for(;;);
+         }  
+         
+         in = Serial.read(); // led1
+         if (in == 0){
+             digitalWrite(LED_1_PIN, LOW);
+         }
+         else if (in == 1){
+             digitalWrite(LED_1_PIN, HIGH);
+         }
+         
+         in = Serial.read(); // led2
+         if (in == 0){
+             digitalWrite(LED_2_PIN, LOW);
+         }
+         else if (in == 1){
+             digitalWrite(LED_2_PIN, HIGH);
+         }
+         
+         in = Serial.read(); // led3
+         if (in == 0){
+             digitalWrite(LED_3_PIN, LOW);
+         }
+         else if (in == 1){
+             digitalWrite(LED_3_PIN, HIGH);
+         }
+         
+         in = Serial.read(); // led4
+         if (in == 0){
+             digitalWrite(LED_4_PIN, LOW);
+         }
+         else if (in == 1){
+             digitalWrite(LED_4_PIN, HIGH);
+         }
+         
+         in = Serial.read(); // 8
+     }
 }
