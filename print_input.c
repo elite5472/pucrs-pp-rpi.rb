4def3502d678f2ee20a4537baa1d1dcd1051b774
elite5472 <guiborges@live.com>
2015-05-19 20:19:42 -0300

    debug

--- print_input.c
+++ print_input.c
@@ -77,7 +77,7 @@ int main(int argc, char** argv){
 			ssize_t bytes_read = read(fd, &data, sizeof(data));
 		
 			if(bytes_read > 0)
-				printf("%c\n", data);
+				printf("0x%h\n", data);
         }
         close(fd);
         return 0;
